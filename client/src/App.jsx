3d35a2f51646eaafa7e726512fb3a1482c188cb6
rknm-cell <racknahm@gmail.com>
2023-07-13 11:11:38 -0400

    created router links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import Home from './Home'
-import Projects from './Projects'
-import About from './About'
-import Contact from './Contact'
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./Home";
+import Projects from "./Projects";
+import About from "./About";
+import Contact from "./Contact";
 function App() {
-  
-
   return (
-    <><Routes>
 
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects /> } />
-      <Route path="/about" element={<About/>} />
-      <Route path="/contact" element={<Contact/>} />
+    // create react routers components connecting to home.jsx, about.jsx, projects.jsx, and contact.jsx
+
+    
+    <>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       
-    </Routes>
+        
+      <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </div>
+    </Router>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
