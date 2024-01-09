11072608cbdd5564bdca92383f91cd1b2dc5ddaa
rknm-cell <racknahm@gmail.com>
2024-01-08 19:59:47 -0500

    starting from scratch

--- client/src/ProjectContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
-import "./styles.css"
-export default function ProjectContainer() {
-  return (
-    <div><ProjectCard/></div>
-  )
-}
