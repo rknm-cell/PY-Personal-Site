18742bc9d27a441809d2b46923a76f5daad364f6
rknm-cell <racknahm@gmail.com>
2024-01-08 22:22:17 -0500

    added mobile responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -19,8 +20,35 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-3 h-3 object-contain"/>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-12 h-12 object-contain mr-3"
+          />
+          <p className="text-white text-[18px]">
+            {" "}
+            Peter Yelton
+            <span className="sm:block hidden">
+              Frontend Developer Full Stack Software Engineer
+            </span>
+          </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            ></li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu} alt="menu" className="w-[28px h-[28px] object-contain cursor-pointer" onClick={() => setToggle=(!toggle)}/>
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[480px] z-10 rounded-xl`}
+
+        </div>
       </div>
     </nav>
   );
